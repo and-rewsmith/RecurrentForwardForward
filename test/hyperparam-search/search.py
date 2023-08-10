3f03c5bab379fd917a26835226bb67a893891c15
Andrew <andrew.smith.recruiting@gmail.com>
2023-08-09 21:18:48 -0700

    fix hyperparam

--- test/hyperparam-search/search.py
+++ test/hyperparam-search/search.py
@@ -11,6 +11,7 @@ from RecurrentFF.util import set_logging
 
 DATA_SIZE = 784
 NUM_CLASSES = 10
+TRAIN_BATCH_SIZE = 500
 TEST_BATCH_SIZE = 5000
 
 
@@ -21,16 +22,6 @@ def run(settings: Settings):
     # Pytorch utils.
     torch.autograd.set_detect_anomaly(True)
     torch.manual_seed(1234)
-
-    data_config = {
-        "data_size": DATA_SIZE,
-        "num_classes": NUM_CLASSES,
-        "train_batch_size": settings.data_config.train_batch_size,
-        "test_batch_size": TEST_BATCH_SIZE,
-        "iterations": iterations}
-
-    if settings.data_config is None:
-        settings.data_config = DataConfig(**data_config)
 
     print(settings.model_dump())
 
@@ -89,10 +80,10 @@ if __name__ == "__main__":
     seen = set()
 
     while True:
+        # random hyperparams
         loss_threshold = random.choice(loss_thresholds)
         iterations_ = random.choice(iterations)
         hidden_sizes_ = random.choice(hidden_sizes)
-
         act = random.choice(ff_act)
         ff_opt = random.choice(ff_optimizers)
         classifier_opt = random.choice(classifier_optimizers)
@@ -110,6 +101,7 @@ if __name__ == "__main__":
         classifier_adadelta_learning_rate = random.choice(
             classifier_adadelta_learning_rates)
 
+        # track id so no dup runs
         unique_run_id = str(loss_threshold) + str(hidden_sizes) + "," + str(act) + "," + \
             str(ff_opt) + "," + str(classifier_opt)
 
@@ -133,26 +125,50 @@ if __name__ == "__main__":
             unique_run_id += "," + \
                 str(classifier_adadelta_learning_rate)
 
+        # construct settings
         settings = Settings.new()
-        settings.model.loss_threshold = loss_threshold
-        settings.model.iterations = iterations_
-        settings.model.hidden_sizes = hidden_sizes_
-        settings.model.ff_act = act
-        settings.model.ff_opt = ff_opt
-        settings.model.classifier_opt = classifier_opt
-        settings.model.ff_rmsprop_momentum = ff_rmsprop_momentum
-        settings.model.ff_rmsprop_learning_rate = ff_rmsprop_learning_rate
-        settings.model.classifier_rmsprop_momentum = classifier_rmsprop_momentum
-        settings.model.classifier_rmsprop_learning_rate = classifier_rmsprop_learning_rate
-        settings.model.ff_adam_learning_rate = ff_adam_learning_rate
-        settings.model.classifier_adam_learning_rate = classifier_adam_learning_rate
-        settings.model.train_batch_size = train_batch_size
-        settings.model.ff_adadelta_learning_rate = ff_adadelta_learning_rate
-        settings.model.classifier_adadelta_learning_rate = classifier_adadelta_learning_rate
 
+        data_config = {
+            "data_size": DATA_SIZE,
+            "num_classes": NUM_CLASSES,
+            "train_batch_size": TRAIN_BATCH_SIZE,
+            "test_batch_size": TEST_BATCH_SIZE,
+            "iterations": iterations_}
+
+        if settings.data_config is None:
+            settings.data_config = DataConfig(**data_config)
+
+        # mutate settings
+        settings.data_config.iterations = iterations_
+        settings.data_config.train_batch_size = train_batch_size
+
+        settings.model.loss_threshold = loss_threshold
+        settings.model.hidden_sizes = hidden_sizes_
+        settings.model.ff_activation = act
+        settings.model.ff_optimizer = ff_opt
+        settings.model.classifier_optimizer = classifier_opt
+
+        if ff_opt == "rmsprop":
+            settings.model.ff_rmsprop.momentum = ff_rmsprop_momentum
+            settings.model.ff_rmsprop.learning_rate = ff_rmsprop_learning_rate
+        elif ff_opt == "adam":
+            settings.model.ff_adam.learning_rate = ff_adam_learning_rate
+        elif ff_opt == "adadelta":
+            settings.model.ff_adadelta.learning_rate = ff_adadelta_learning_rate
+
+
+        if classifier_opt == "rmsprop":
+            settings.model.classifier_rmsprop.momentum = classifier_rmsprop_momentum
+            settings.model.classifier_rmsprop.learning_rate = classifier_rmsprop_learning_rate
+        elif classifier_opt == "adam":
+            settings.model.classifier_adam.learning_rate = classifier_adam_learning_rate
+        elif classifier_opt == "adadelta":
+            settings.model.classifier_adadelta.learning_rate = classifier_adadelta_learning_rate
+
+        # run hyperparams
         if unique_run_id not in seen:
             p = Process(target=run, args=(
-                settings
+                settings,
             ))
             p.start()
             p.join()
