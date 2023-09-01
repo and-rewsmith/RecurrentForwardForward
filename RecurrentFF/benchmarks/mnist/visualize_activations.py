ea7b372f6104ec59db7e6c05fc9dc2130dbc48ae
Andrew <andrew.smith.recruiting@gmail.com>
2023-09-01 11:33:59 -0700

    progress

--- RecurrentFF/benchmarks/mnist/visualize_activations.py
+++ RecurrentFF/benchmarks/mnist/visualize_activations.py
@@ -5,16 +5,22 @@ import matplotlib.pyplot as plt
 
 tensors = torch.load('test_sample_1.pt')
 
-correct = tensors['incorrect_activations']
-print(correct.shape)
+scenario = "correct_activations"
+loaded = tensors[scenario]
+print(loaded.shape)
+timesteps = loaded.shape[0]
 
-for i in range(0, correct.shape[0]):
-    print(correct[i].shape)
-    input()
-    df = pd.DataFrame(correct[i].cpu().numpy())
-    plt.figure(figsize=(10, 5))
-    sns.heatmap(df, cmap='viridis')
-    plt.title('Activation Heatmap')
-    plt.xlabel('Neuron #')
-    plt.ylabel('Layers')
-    plt.show()
+fig, axes = plt.subplots(timesteps, 1, figsize=(10, 5 * timesteps))
+
+for t in range(timesteps):
+    df = pd.DataFrame(loaded[t].cpu().numpy())
+    sns_heatmap = sns.heatmap(df, cmap='viridis', cbar_kws={
+                              'label': 'Activation Value'}, ax=axes[t])
+    cbar = sns_heatmap.collections[0].colorbar
+    cbar.set_label('Activation Value', fontsize=12)
+    axes[t].set_title(f'Timestep {t}', fontsize=12)
+    axes[t].set_xlabel('Neuron #', fontsize=12)
+    axes[t].set_ylabel('Layer', fontsize=12)
+
+plt.tight_layout()
+plt.savefig(f"{scenario}.png", dpi=300)
