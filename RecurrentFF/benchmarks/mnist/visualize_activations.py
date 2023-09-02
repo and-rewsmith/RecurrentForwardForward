d9cf49a5cc986826f6bef3dfcedeba8c5df190e3
Andrew <andrew.smith.recruiting@gmail.com>
2023-09-01 17:01:06 -0700

    better vis

--- RecurrentFF/benchmarks/mnist/visualize_activations.py
+++ RecurrentFF/benchmarks/mnist/visualize_activations.py
@@ -7,25 +7,39 @@ BASE_PATH = "./img/activation_heatmaps"
 
 tensors = torch.load('test_sample_1.pt')
 
-scenario = "correct_activations"
-loaded = tensors[scenario]
-print(loaded.shape)
-timesteps = loaded.shape[0]
+scenarios = ["incorrect_activations", "correct_activations"]
 
-fig, axes = plt.subplots(timesteps, 1, figsize=(10, 5 * timesteps))
+global_max = 0
+for scenario in scenarios:
+    loaded = tensors[scenario]
+    timesteps = loaded.shape[0]
 
-for t in range(timesteps):
-    timestep_tensor = loaded[t]
-    timestep_tensor = torch.abs(timestep_tensor)
+    for t in range(timesteps):
+        timestep_tensor = loaded[t]
+        timestep_tensor = torch.abs(timestep_tensor)
 
-    df = pd.DataFrame(timestep_tensor.cpu())
-    sns_heatmap = sns.heatmap(df, cmap='viridis', cbar_kws={
-                              'label': 'Activation Value'}, ax=axes[t])
-    cbar = sns_heatmap.collections[0].colorbar
-    cbar.set_label('Activation Value', fontsize=12)
-    axes[t].set_title(f'Timestep {t}', fontsize=12)
-    axes[t].set_xlabel('Neuron #', fontsize=12)
-    axes[t].set_ylabel('Layer', fontsize=12)
+        max_from_timestep = torch.max(timestep_tensor).item()
+        if max_from_timestep > global_max:
+            global_max = max_from_timestep
 
-plt.tight_layout()
-plt.savefig(f"{BASE_PATH}/{scenario}.png", dpi=300)
+for scenario in scenarios:
+    loaded = tensors[scenario]
+    timesteps = loaded.shape[0]
+
+    fig, axes = plt.subplots(timesteps, 1, figsize=(10, 5 * timesteps))
+
+    for t in range(timesteps):
+        timestep_tensor = loaded[t]
+        timestep_tensor = torch.abs(timestep_tensor)
+
+        df = pd.DataFrame(timestep_tensor.cpu())
+        sns_heatmap = sns.heatmap(df, cmap='viridis', vmin=0, vmax=global_max, cbar_kws={
+            'label': 'Activation Value'}, ax=axes[t])
+        cbar = sns_heatmap.collections[0].colorbar
+        cbar.set_label('Activation Value', fontsize=12)
+        axes[t].set_title(f'Timestep {t}', fontsize=12)
+        axes[t].set_xlabel('Neuron #', fontsize=12)
+        axes[t].set_ylabel('Layer', fontsize=12)
+
+    plt.tight_layout()
+    plt.savefig(f"{BASE_PATH}/{scenario}.png", dpi=300)
