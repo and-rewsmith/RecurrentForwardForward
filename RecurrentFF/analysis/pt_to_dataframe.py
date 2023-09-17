0af7c8490ab241d85aa1a8f5759afc272ca4e12a
Andrew <andrew.smith.recruiting@gmail.com>
2023-09-17 01:35:14 -0700

    dataframe

--- RecurrentFF/analysis/pt_to_dataframe.py
+++ RecurrentFF/analysis/pt_to_dataframe.py
@@ -5,17 +5,18 @@ import os
 # Define constants
 DATASET = "MNIST"
 LABEL_SHOW_BOUNDARY = 10
-NUMBER_FILES = 1
+NUMBER_FILES = 100
 DIR_PATH = "artifacts/activations"
 DEVICE = "cuda"
+BATCH_SIZE = 1000  # You can adjust this value based on your memory availability
 
-
-# Get the first 100 files from the directory
+# Get the first NUMBER_FILES files from the directory
 files = [f for f in os.listdir(DIR_PATH) if f.endswith('.pt')]
 files = sorted(files)[:NUMBER_FILES]
 
-# Initialize an empty dataframe
+# Initialize an empty dataframe and a list for batching
 df = pd.DataFrame()
+batch_rows = []
 
 for i, file in enumerate(files):
     print("processing file", i, "of", len(files))
@@ -28,7 +29,6 @@ for i, file in enumerate(files):
         for activity_type in ["activations", "forward_activations", "backward_activations", "lateral_activations"]:
             activity_data = data[f"{data_type}_{activity_type}"]
 
-            # Determine the activation type based on the activity_type
             if activity_type == "activations":
                 activation_type_name = "full"
             else:
@@ -47,19 +47,26 @@ for i, file in enumerate(files):
                             "layer_index": layer_index,
                             "is_label_showing": is_label_showing,
                             "neuron index": neuron_index,
-                            "activity": neuron_data.cpu().item(),  # Convert to scalar
-                            "image": data['data'][timestep].cpu().numpy(),
-                            # static single class means 0 index is safe
-                            "label": data['labels'][0].cpu().numpy(),
+                            "activity": neuron_data.cpu().item(),
+                            "image": ','.join(map(str, data['data'][timestep].cpu().numpy().flatten())),
+                            "label": data['labels'][0].cpu().numpy().flatten(),
                             "dataset": DATASET,
                             "is_correct": True if data_type == "correct" else False,
                             "activation_type": activation_type_name
                         }
-                        df = pd.concat(
-                            [df, pd.DataFrame([row])], ignore_index=True)
+                        batch_rows.append(row)
 
-# Save dataframe to a csv file
+                        # If batch size is reached, add it to the main dataframe
+                        if len(batch_rows) == BATCH_SIZE:
+                            df = pd.concat(
+                                [df, pd.DataFrame(batch_rows)], ignore_index=True)
+                            batch_rows.clear()
+
+# Add any remaining rows in the batch_rows to the main dataframe
+if batch_rows:
+    df = pd.concat([df, pd.DataFrame(batch_rows)], ignore_index=True)
+
+# Save dataframe to a parquet file
 df.to_parquet('converted_data.parquet')
-
 
 print("Data conversion complete.")
