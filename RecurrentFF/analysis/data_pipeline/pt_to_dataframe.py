7dee956c25c616ab749ce7099802cafaa191c320
Andrew <andrew.smith.recruiting@gmail.com>
2023-09-19 21:03:24 -0700

    pt

--- RecurrentFF/analysis/data_pipeline/pt_to_dataframe.py
+++ RecurrentFF/analysis/data_pipeline/pt_to_dataframe.py
@@ -6,16 +6,14 @@ import os
 DATASET = "MNIST"
 LABEL_SHOW_BOUNDARY = 10
 NUMBER_FILES = 100
-DIR_PATH = "artifacts/activations_917"
-DEVICE = "mps"
-# You can adjust this value based on your memory availability
+DIR_PATH = "artifacts/activations"
+DEVICE = "cuda"
 BATCH_SIZE = 1024 ** 3
 
 # Get the first NUMBER_FILES files from the directory
 files = [f for f in os.listdir(DIR_PATH) if f.endswith('.pt')]
 files = sorted(files)[:NUMBER_FILES]
 
-# Initialize an empty dataframe and a list for batching
 df = pd.DataFrame()
 batch_rows = []
 
@@ -25,51 +23,41 @@ for i, file in enumerate(files):
     # Load data
     data = torch.load(os.path.join(DIR_PATH, file), map_location=DEVICE)
 
-    # Extract data from the loaded file
     for data_type, label in [("correct", 1), ("incorrect", 0)]:
-        for activity_type in ["activations", "forward_activations", "backward_activations", "lateral_activations"]:
-            activity_data = data[f"{data_type}_{activity_type}"]
+        for timestep, _ in enumerate(data[f"{data_type}_activations"]):
+            is_label_showing = 1 if timestep >= LABEL_SHOW_BOUNDARY else 0
 
-            if activity_type == "activations":
-                activation_type_name = "full"
-            else:
-                activation_type_name = activity_type.replace(
-                    "_activations", "")
+            print("processing timestep ", timestep)
 
-            for timestep, timestep_data in enumerate(activity_data):
-                is_label_showing = 1 if timestep >= LABEL_SHOW_BOUNDARY else 0
+            for layer_index, _ in enumerate(data[f"{data_type}_activations"][timestep]):
+                for neuron_index, _ in enumerate(data[f"{data_type}_activations"][timestep][layer_index]):
+                    row = {
+                        "image_timestep": timestep,
+                        "layer_index": layer_index,
+                        "is_label_showing": is_label_showing,
+                        "neuron index": neuron_index,
+                        "image": ','.join(map(str, data['data'][timestep].cpu().numpy().flatten())),
+                        "label": data['labels'][0].cpu().numpy().flatten(),
+                        "dataset": DATASET,
+                        "is_correct": True if data_type == "correct" else False,
+                        "data_sample_id": i,
+                        "activation": data[f"{data_type}_activations"][timestep][layer_index][neuron_index].cpu().item(),
+                        "forward_activation_component": data[f"{data_type}_forward_activations"][timestep][layer_index][neuron_index].cpu().item(),
+                        "backward_activation_component": data[f"{data_type}_backward_activations"][timestep][layer_index][neuron_index].cpu().item(),
+                        "lateral_activation_component": data[f"{data_type}_lateral_activations"][timestep][layer_index][neuron_index].cpu().item()
+                    }
+                    batch_rows.append(row)
 
-                print("processing timestep ", timestep)
+                    if len(batch_rows) == BATCH_SIZE:
+                        df = pd.concat(
+                            [df, pd.DataFrame(batch_rows)], ignore_index=True)
+                        batch_rows.clear()
+                        df.to_parquet('converted_data.parquet')
+                        print("saved batch to parquet file")
 
-                for layer_index, layer_data in enumerate(timestep_data):
-                    for neuron_index, neuron_data in enumerate(layer_data):
-                        row = {
-                            "image_timestep": timestep,
-                            "layer_index": layer_index,
-                            "is_label_showing": is_label_showing,
-                            "neuron index": neuron_index,
-                            "activity": neuron_data.cpu().item(),
-                            "image": ','.join(map(str, data['data'][timestep].cpu().numpy().flatten())),
-                            "label": data['labels'][0].cpu().numpy().flatten(),
-                            "dataset": DATASET,
-                            "is_correct": True if data_type == "correct" else False,
-                            "activation_type": activation_type_name
-                        }
-                        batch_rows.append(row)
-
-                        # If batch size is reached, add it to the main dataframe
-                        if len(batch_rows) == BATCH_SIZE:
-                            df = pd.concat(
-                                [df, pd.DataFrame(batch_rows)], ignore_index=True)
-                            batch_rows.clear()
-                            df.to_parquet('converted_data.parquet')
-                            print("saved batch to parquet file")
-
-# Add any remaining rows in the batch_rows to the main dataframe
 if batch_rows:
     df = pd.concat([df, pd.DataFrame(batch_rows)], ignore_index=True)
 
-# Save dataframe to a parquet file
 df.to_parquet('converted_data.parquet')
 
 print("Data conversion complete.")
