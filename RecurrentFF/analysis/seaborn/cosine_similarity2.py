1b8035ca5d61019098d04fdb8612580843d7c4df
Andrew <andrew.smith.recruiting@gmail.com>
2023-09-22 22:30:32 -0700

    flip

--- RecurrentFF/analysis/seaborn/cosine_similarity2.py
+++ RecurrentFF/analysis/seaborn/cosine_similarity2.py
@@ -22,13 +22,20 @@ COMPLEX_COMPARISONS = [
 ]
 
 
-def compute_cosine_similarity(df):
+def compute_cosine_similarity(df, is_correct):
     # Define the list of comparisons
 
     all_comparisons = BASIC_COMPARISONS + COMPLEX_COMPARISONS
 
+    # filter df by is_correct
+    # print("----compute cosine sim")
+    # print(df.shape)
+    df = df[df['is_correct'] == is_correct]
+    # print(df.shape)
     df = df[['image_timestep', 'neuron index', 'forward_activation_component',
             'backward_activation_component', 'lateral_activation_component']]
+    # print(df.columns)
+    # print(df.shape)
 
     # print(df.shape)
     # print(df.columns)
@@ -36,6 +43,9 @@ def compute_cosine_similarity(df):
     #       'lateral_activation_component']].values.shape)
     # print(df[['forward_activation_component', 'backward_activation_component',
     #           'lateral_activation_component']].columns)
+    # input()
+
+    # print(df.columns)
     # input()
 
     forward_tensor = df.pivot(index='image_timestep', columns='neuron index',
@@ -49,10 +59,10 @@ def compute_cosine_similarity(df):
     concatenated_tensor = np.concatenate(
         (forward_tensor, backward_tensor, lateral_tensor), axis=0)
 
-    # print(forward_tensor.shape)
-    # print(backward_tensor.shape)
-    # print(lateral_tensor.shape)
-    # print(concatenated_tensor.shape)
+    print(forward_tensor.shape)
+    print(backward_tensor.shape)
+    print(lateral_tensor.shape)
+    print(concatenated_tensor.shape)
     # input()
 
     # Use PCA for dimensionality reduction
@@ -100,17 +110,15 @@ def compute_cosine_similarity(df):
     return cos_sim_results
 
 
-def plot_cosine_similarity(df, is_correct):
+def plot_cosine_similarity(df):
 
     sns.set_style("whitegrid")  # Set Seaborn style
 
-    # Filter the dataframe based on is_correct
-    df_filtered = df[df['is_correct'] == is_correct]
-    if 'image' in df.columns:
-        df_filtered = df_filtered.drop(columns=['image', 'dataset'])
+    df = df.drop(columns=['image', 'dataset'])
 
-    df_grouped = df_filtered.groupby(
-        ['layer_index', 'neuron index', 'image_timestep']).mean().reset_index()
+    df_grouped = df.groupby(
+        ['layer_index', 'neuron index', 'image_timestep', 'is_correct']).mean().reset_index()
+    print(df_grouped.shape)
 
     n_layers = df['layer_index'].nunique()
 
@@ -119,32 +127,55 @@ def plot_cosine_similarity(df, is_correct):
 
     for layer in df['layer_index'].unique():
         df_layer = df_grouped[df_grouped['layer_index'] == layer]
-        cos_sims = compute_cosine_similarity(df_layer)
+        print(df_layer.shape)
+        cos_sims_pos = compute_cosine_similarity(df_layer, True)
+        cos_sims_neg = compute_cosine_similarity(df_layer, False)
 
-        for comparison, cos_sim in cos_sims.items():
+        for comparison, cos_sim in cos_sims_pos.items():
             for i, val in enumerate(cos_sim):
                 plot_data.append({
                     'layer': layer,
                     'timestep': i,
                     'similarity': val,
-                    'comparison': f'{comparison[0].split("_")[0]} vs {comparison[1].split("_")[0]}',
-                    'type': 'Basic' if comparison in BASIC_COMPARISONS else 'Complex'
+                    'comparison': f'{comparison[0].split("_")[0]} vs {comparison[1].split("_")[0] if "+" not in comparison[1] else "(" + comparison[1].split("+")[0].split("_")[0] + " + " + comparison[1].split("+")[1].split("_")[0] + ")"}',
+                    'type': 'Basic' if comparison in BASIC_COMPARISONS else 'Complex',
+                    'is_pos_data': True
+                })
+
+        for comparison, cos_sim in cos_sims_neg.items():
+            for i, val in enumerate(cos_sim):
+                plot_data.append({
+                    'layer': layer,
+                    'timestep': i,
+                    'similarity': val,
+                    'comparison': f'{comparison[0].split("_")[0]} vs {comparison[1].split("_")[0] if "+" not in comparison[1] else "(" + comparison[1].split("+")[0].split("_")[0] + " + " + comparison[1].split("+")[1].split("_")[0] + ")"}',
+                    'type': 'Basic' if comparison in BASIC_COMPARISONS else 'Complex',
+                    'is_pos_data': False
                 })
 
     # Convert the list to a DataFrame
     plot_df = pd.DataFrame(plot_data)
 
-    # Using Seaborn's FacetGrid
-    g = sns.FacetGrid(plot_df, col="layer", row="type", height=5, aspect=1.2)
-    g = g.map(sns.lineplot, 'timestep', 'similarity',
-              'comparison', palette='tab10')
-    g.add_legend()
+    # Separate the data into basic and complex
+    df_basic = plot_df[plot_df['type'] == 'Basic']
+    df_complex = plot_df[plot_df['type'] == 'Complex']
 
-    # Save the plot
-    g.savefig(
-        f"img/presentation/cosine_sim/{'correct' if is_correct else 'incorrect'}.png")
+    # Plotting for basic comparisons
+    g_basic = sns.FacetGrid(df_basic, col="layer",
+                            row="is_pos_data", height=5, aspect=1.2)
+    g_basic = g_basic.map(sns.lineplot, 'timestep',
+                          'similarity', 'comparison', palette='tab10')
+    g_basic.add_legend()
+    g_basic.savefig("img/presentation/cosine_sim/basic_comparisons.png")
+
+    # Plotting for complex comparisons
+    g_complex = sns.FacetGrid(df_complex, col="layer",
+                              row="is_pos_data", height=5, aspect=1.2)
+    g_complex = g_complex.map(sns.lineplot, 'timestep',
+                              'similarity', 'comparison', palette='tab10')
+    g_complex.add_legend()
+    g_complex.savefig("img/presentation/cosine_sim/complex_comparisons.png")
 
 
 df = pd.read_parquet('~/Downloads/dataframe_9-19.parquet')
-plot_cosine_similarity(df, True)
-plot_cosine_similarity(df, False)
+plot_cosine_similarity(df)
