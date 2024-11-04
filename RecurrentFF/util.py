20b6d359e199abf2d3d8defff399443ae1f08cb9
Andrew Smith <andrew.smith.recruiting@gmail.com>
2024-11-04 12:11:25 -0800

    prog

--- RecurrentFF/util.py
+++ RecurrentFF/util.py
@@ -211,13 +211,13 @@ def set_logging() -> None:
 
 
 def standardize_layer_activations(layer_activations: torch.Tensor, epsilon: float = 0.000000000001) -> torch.Tensor:
-    # squared_activations = layer_activations ** 2
-    # mean_squared = torch.mean(squared_activations, dim=1, keepdim=True)
-    # l2_norm = torch.sqrt(mean_squared + epsilon)
+    squared_activations = layer_activations ** 2
+    mean_squared = torch.mean(squared_activations, dim=1, keepdim=True)
+    l2_norm = torch.sqrt(mean_squared + epsilon)
 
-    # normalized_activations = layer_activations / l2_norm
-    # return normalized_activations
-    return layer_activations
+    normalized_activations = layer_activations / l2_norm
+    return normalized_activations
+    # return layer_activations
 
 
 class TrainTestBridgeFormatLoader:
